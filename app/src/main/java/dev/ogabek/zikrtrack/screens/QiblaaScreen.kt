12e4aed8963586497cf4a8d0abc3fcf706817ffd
Ogabek Matyakubov <OgabekDev@gmail.com>
2024-12-04 01:40:30 +0500

    Kabaa Background and topbar added

--- app/src/main/java/dev/ogabek/zikrtrack/screens/QiblaaScreen.kt
+++ app/src/main/java/dev/ogabek/zikrtrack/screens/QiblaaScreen.kt
@@ -1,9 +1,45 @@
 package dev.ogabek.zikrtrack.screens
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.navigation.NavHostController
+import dev.ogabek.zikrtrack.R
+import dev.ogabek.zikrtrack.utils.TasbihDetailsBackground
+import dev.ogabek.zikrtrack.utils.TopBar
 
 @Composable
 fun QiblaaScreen(navController: NavHostController) {
 
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        TasbihDetailsBackground()
+        KabaaView()
+    }
+
+}
+
+@Composable
+fun KabaaView() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Transparent)
+    ) {
+        TopBar(
+            title = stringResource(R.string.kabaa),
+        )
+
+        Box {
+
+        }
+
+    }
 }
